ffe7b4587e08c194d3b6bac69a8c029ed315f0ab
SilverPeak2299 <danny@192-168-1-11.tpgi.com.au>
2025-04-25 18:07:17 +1000

    task 2_2 prgress

--- task1_2.py
+++ task1_2.py
@@ -4,10 +4,10 @@ from mrjob.job import MRJob
 class CoffeeSalesSum(MRJob):
     def mapper(self, _, line):
         transaction = line.split("\t")
-        yield transaction[0], float(transaction[1])
+        yield transaction[0], float(transaction[2])
 
     def reducer(self, name, cost):
-        yield name, sum(cost)
+        yield name, round(sum(cost), 2)
 
 
 if __name__ == "__main__":
--- task2_1.py
+++ task2_1.py
@@ -9,15 +9,15 @@ smallest_sales = total_sales.nsmallest(3, 'Sales')["Coffee"].tolist()
 class LeastSoldCoffee(MRJob):
 
     def steps(self):
-        return [MRStep(mapper= self.mapper_1, reducer= self.reducer_1),
-                MRStep(mapper= self.mapper_2, reducer= self.reducer_2)]
+        return [MRStep(mapper= self.mapper_1, reducer= self.reducer_1)]
+                #MRStep(mapper= self.mapper_2, reducer= self.reducer_2)]
     
     def mapper_1(self, _, line):
         transaction = line.split("\t")
-        yield transaction[0]+": " +transaction[5]+ "-" +transaction[4] ,float(transaction[1])
+        yield transaction[0]+": " +transaction[5]+ "-" +transaction[4] ,float(transaction[2])
 
     def reducer_1(self, coffee_type, cost):
-        yield coffee_type, sum(cost)
+        yield coffee_type, round(sum(cost), 2)
 
     def mapper_2(self, coffee_sales,summed_cost):
         yield None, (coffee_sales, summed_cost)
